33c80b422a1318024150f815cbfce250318ae61c
Jeff Stern <jeffstern.ny@gmail.com>
2015-10-09 23:41:08 -0400

    Added code to download images

--- scrape.py
+++ scrape.py
@@ -47,32 +47,69 @@ def get_tags(site):
 	else:
 		return []
 
-output_file = open("collecteddata_"+get_current_date_string()+".csv", 'wb')
-output = csv.writer(output_file)
-output.writerow(["URL", "Text", "Links", "Image URLS", "Image Alt Text","Tags"])
-
-for line in open("urllist.txt", 'r'):	
+def download_photo(img_url, filename):
+	# http://stackoverflow.com/questions/3042757/downloading-a-picture-via-urllib-and-python
 	try:
-		site = parse_html(line)
-		copy = get_copy(site)
-		copyText = ""
-		copyLinksHref = []
-		if copy:
-			copyText = convert_to_plain_text(copy)
-			copyLinksHref = get_all_attr(copy.find_all("a"), "href")
-		tags = get_tags(site)
-		images = get_images(site)
-		if images:
-			imagesSrc = get_all_attr(images, "src")
-			imagesAlt = get_all_attr(images, "alt")
-
-		data = [line, copyText, copyLinksHref, imagesSrc, imagesAlt,tags]
-		output.writerow([unicode(s).encode("utf-8") for s in data])
-
-		print(line)
+	    image_on_web = urllib2.urlopen(img_url)
+	    if image_on_web.headers.maintype == 'image':
+	        buf = image_on_web.read()
+	        path = os.getcwd() + "/"
+	        file_path = "%s%s" % (path, filename)
+	        downloaded_image = file(file_path, "wb")
+	        downloaded_image.write(buf)
+	        downloaded_image.close()
+	        image_on_web.close()
+	    else:
+			return False    
 	except:
-		print("FAILED")
-		print(line)
-		sys.exit(0)
+		print("URL for "+ img_url + " not found")
+		return False
+	return True
 
-output_file.close()
+def create_csv_with_metadata():
+	output_file = open("collecteddata_"+get_current_date_string()+".csv", 'wb')
+	output = csv.writer(output_file)
+	output.writerow(["URL", "Text", "Links", "Image URLS", "Image Alt Text","Tags"])
+
+	for line in open("urllist.txt", 'r'):	
+		try:
+			site = parse_html(line)
+			copy = get_copy(site)
+			copyText = ""
+			copyLinksHref = []
+			if copy:
+				copyText = convert_to_plain_text(copy)
+				copyLinksHref = get_all_attr(copy.find_all("a"), "href")
+			tags = get_tags(site)
+			images = get_images(site)
+			if images:
+				imagesSrc = get_all_attr(images, "src")
+				imagesAlt = get_all_attr(images, "alt")
+
+			data = [line, copyText, copyLinksHref, imagesSrc, imagesAlt,tags]
+			output.writerow([unicode(s).encode("utf-8") for s in data])
+
+			print(line)
+		except:
+			print("Failed - likely because of special character in URL")
+			print(line)
+			sys.exit(0)
+
+	output_file.close()
+
+#create_csv_with_metadata()
+
+lineNumber = 0
+for line in open("urllist.txt", 'r'):
+	site = parse_html(line)
+	images = get_images(site)
+	imageNumber = 0
+	if images:
+		imagesSrc = get_all_attr(images, "src")
+		for src in imagesSrc:
+			fileExtension = src[src.rfind("."):]
+			fileName =  "post"+str(lineNumber)+"_image"+str(imageNumber) + fileExtension
+			download_photo(src, fileName)
+			imageNumber += 1
+	lineNumber +=1
+
